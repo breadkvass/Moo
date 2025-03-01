d6300ad6d4d9de98f443fa29093524cb5f0fbdf2
Anastasiya Sulimova <sulimova.a.m@gmail.com>
2025-03-01 18:27:48 +0300

    add routing to LoginPage

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,16 +1,26 @@
 import { FC } from 'react';
 import styles from './layout.module.css';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 type LayoutProps = {
     children: React.JSX.Element;
 }
 
 const Layout: FC<LayoutProps> = ({children}) => {
+    const navigate = useNavigate();
+    const pathname = useLocation().pathname;
+
+    const route = (path: string) => {
+        if (pathname !== path) {
+            navigate(path);
+        } else return
+    }
+
     return (
         <div className={styles.layout}>
             <div className={styles.buttons}>
-                <button className={styles.button}>About us</button>
-                <button className={styles.button}>Sign in</button>
+                <button className={styles.button} onClick={() => route('/')}>About us</button>
+                <button className={styles.button} onClick={() => route('/login')}>Sign in</button>
                 {/* <button className={styles.button}></button> */}
             </div>
             {children}
