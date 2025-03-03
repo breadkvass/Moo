24fecdab2be9d5e0b65bc5cc8d849e72602cad3e
Anastasiya Sulimova <sulimova.a.m@gmail.com>
2025-03-03 15:40:44 +0300

    profile-page: add ProfilePage

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import {
 } from "react-router-dom";
 import MainPage from "./pages/mainPage/mainPage";
 import LoginPage from "./pages/loginPage/loginPage";
+import ProfilePage from "./pages/profilePage/profilePage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route path="/" element={<MainPage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
     </Route>
   )
 );
