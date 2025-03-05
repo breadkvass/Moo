bde83a0b90c56af3586c664bc7492e899e3e1e65
Anastasiya Sulimova <sulimova.a.m@gmail.com>
2025-03-05 15:27:26 +0300

    modal: add modal to profile-page

--- src/pages/profilePage/profilePage.tsx
+++ src/pages/profilePage/profilePage.tsx
@@ -3,31 +3,44 @@ import { AuthContext } from '../../utils/authContext';
 import Layout from '../../components/layout/layout';
 import Avatar from '../../assets/avatar.jpg';
 import SpinnerIcon from '../../components/spinnerIcon/spinnerIcon';
+import Modal from '../../components/modal/modal';
+import { ModalContext } from '../../hooks/useModal/useModalProvider';
 import styles from './profilePage.module.css';
 
 const ProfilePage = () => {
     const [ user ] = useContext(AuthContext);
+    const [ openModal, closeModal ] = useContext(ModalContext);
     const [ isLoading, setIsLoading ] = useState(false);
     const [ userName, setUserName ] = useState('');
 
     useEffect(() => {
         setIsLoading(true);
-        if (user.user.fullname) setIsLoading(false)
         if (user.user.fullname) {
             const userName = user.user.fullname.split(' ')[0];
             setIsLoading(false)
             setUserName(userName)
         }
+    }, [user])
+
+    const stopRequestHandler = () => {
+        closeModal();
+    }
+
+    const onUpdateHadler = () => {
+        openModal(
+            <Modal closeHandler={closeModal} cancelHandler={stopRequestHandler} />
+        )
+    }
 
     return (
         <Layout>
-            {isLoading ? <SpinnerIcon spinnerStyle='' /> :
+            {isLoading ? <SpinnerIcon /> :
                 <div className={styles.page}>
                     <div className={styles.profile}> 
                         <img className={styles.avatar} src={Avatar} alt='avatar' />
                         <div className={styles.info}>
                             <p className={styles.name}>Welcome, {userName}!</p>
-                            <button className={styles.update}>Update</button>
+                            <button className={styles.update} onClick={() => onUpdateHadler()}>Update</button>
                         </div>
                     </div>
                     <p className={styles.quote}></p>
